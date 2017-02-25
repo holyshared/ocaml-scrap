45a0bb2674a0c6d759499880ae425445894559ec
holyshared <holy.shared.design@gmail.com>
2017-02-25 15:51:28 +0900

    add

--- example_lwt/src/example_lwt.ml
+++ example_lwt/src/example_lwt.ml
@@ -7,6 +7,15 @@ let map_thread =
   let started_message s = "started: " ^ s in
   Lwt.map started_message root
 
+let mulit_threads =
+  let tasks = [
+    Lwt.return "thread - 1";
+    Lwt.return "thread - 2";
+    Lwt.return "thread - 3";
+  ] in
+  Lwt.nchoose tasks
+
 let () =
   print_endline (Lwt_main.run simple_thread);
   print_endline (Lwt_main.run map_thread);
+  List.iter (fun s -> print_endline s) (Lwt_main.run mulit_threads)
