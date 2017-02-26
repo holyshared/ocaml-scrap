555560c056723790397a982a043cb5440946f869
holyshared <holy.shared.design@gmail.com>
2017-02-26 18:44:54 +0900

    add map functon

--- example_github/src/pull_request.ml
+++ example_github/src/pull_request.ml
@@ -8,11 +8,18 @@ let string_of pull_number =
 
 (** print pull request number of github repository *)
 let print_of p =
+  Monad.return (print_endline (string_of p))
+
+let number_of p =
   let open Github_t in
-  Monad.return (print_endline (string_of p.pull_number))
+  Monad.return [p.pull_number]
+
+let pull_requests ~user ~repo =
+  let results = Pull.for_repo ~user ~repo ~state:`Open () in
+  Stream.map number_of results
 
 (** print all opened pull request number of github repository *)
 let print_of_pull_requests ~user ~repo =
-  let pull_requests = Pull.for_repo ~user ~repo ~state:`Open () in
-  let print_pull_requests = Stream.iter print_of pull_requests in
+  let number_of_stream = pull_requests ~user ~repo in
+  let print_pull_requests = Stream.iter print_of number_of_stream in
   Lwt_main.run (Monad.run print_pull_requests)
