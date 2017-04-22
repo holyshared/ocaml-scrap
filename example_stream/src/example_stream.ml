5aafbbe4ed690cd38a1c2a9bd6899c8c80e945da
holyshared <holy.shared.design@gmail.com>
2017-04-22 15:12:27 +0900

    replace to pipe operator

--- example_stream/src/example_stream.ml
+++ example_stream/src/example_stream.ml
@@ -1,12 +1,18 @@
 let print_lines t =
   Line_stream.iter t ~f:(fun v -> print_endline v)
 
-let () =
-  let input_stream file = Line_stream.of_file file in
-  match input_stream "fixtures/input.txt" with
-    | Error e -> print_endline e
-    | Ok s -> print_lines s;
+let line_stream_of file = Line_stream.of_file file
 
-  match input_stream "fixtures/input.txt" with
+let print_all_lines o =
+  match o with
+    | Error e -> print_endline e
+    | Ok s -> print_lines s
+
+let print_iter_n o ~n =
+  match o with
     | Error e -> print_endline e
     | Ok s -> Line_stream.iter_n s ~n:2 ~f:(fun ss -> List.iter (fun s -> print_endline s) ss)
+
+let () =
+  line_stream_of "fixtures/input.txt" |> print_all_lines;
+  line_stream_of "fixtures/input.txt" |> print_iter_n ~n:2
