8fcd775f75412c64ac03a91b35c1a97bdf0393d3
holyshared <holy.shared.design@gmail.com>
2017-06-24 20:20:02 +0900

    add tcp_header parser

---
+++ example_tcp_header/src/example_tcp_header.ml
@@ -0,0 +1,15 @@
+open Tcp_header
+
+let () =
+  let header = parse "tests/fixtures/tcp_header.byte" in
+  print_endline (string_of_int header.src_port);
+  print_endline (string_of_int header.dest_port);
+  print_endline (string_of_bool header.urgent);
+  print_endline (string_of_bool header.ack);
+  print_endline (string_of_bool header.push);
+  print_endline (string_of_bool header.reset);
+  print_endline (string_of_bool header.syn);
+  print_endline (string_of_bool header.fin);
+  print_endline (string_of_int header.window_size);
+  print_endline (string_of_int header.checksum);
+  print_endline (string_of_int header.urgent_pointer);
--- example_tcp_header/src/tcp_header.ml
+++ example_tcp_header/src/tcp_header.ml
@@ -1,3 +1,21 @@
+type tcp_header = {
+  src_port: int;
+  dest_port: int;
+  seq_number: int;
+  ack_number: int;
+  data_offset: int;
+  receive: int;
+  urgent: bool;
+  ack: bool;
+  push: bool;
+  reset: bool;
+  syn: bool;
+  fin: bool;
+  window_size: int;
+  checksum: int;
+  urgent_pointer: int;
+}
+
 type input = {
   pos_in: unit -> int;
   seek_in: int -> unit;
@@ -22,6 +40,13 @@ let input_of_channel ch =
 let align b =
   b.bit <- 0
 
+let input_bits_of_input i =
+  {
+    input = i;
+    byte = 0;
+    bit = 0
+  }
+
 (* take 1 bit from buffer *)
 let rec getbit b =
   if b.bit = 0 then
@@ -36,3 +61,38 @@ let rec getbit b =
     let r = b.byte land b.bit > 0 in
     b.bit <- b.bit / 2; (* 128, 64, 32, 16, 8, 4, 2 ... *)
     r
+
+let getval b n =
+  if n <= 0 || n > 31 then
+    raise (Invalid_argument "getval")
+  else
+    let r = ref 0 in
+    for x = n- 1 downto 0 do
+      r := !r lor ((if getbit b then 1 else 0) lsl x)
+    done;
+    !r
+
+let input_bits_of_file f =
+  let channel = open_in_bin f in
+  let input = input_of_channel channel in
+  input_bits_of_input input
+
+let parse f =
+  let input = input_bits_of_file f in
+  {
+    src_port = getval input 16;
+    dest_port = getval input 16;
+    seq_number = getval input 31;
+    ack_number = getval input 31;
+    data_offset = getval input 4;
+    receive = getval input 6;
+    urgent = getbit input;
+    ack = getbit input;
+    push = getbit input;
+    reset = getbit input;
+    syn = getbit input;
+    fin = getbit input;
+    window_size = getval input 16;
+    checksum = getval input 16;
+    urgent_pointer = getval input 16;
+  }
