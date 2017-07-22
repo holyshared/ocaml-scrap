05ef611cfcb2bb868338a283c9719cd70d4deb80
holyshared <holy.shared.design@gmail.com>
2017-07-22 15:13:42 +0900

    add buffer exec

--- example_lwt_command/src/stream/example_stream.ml
+++ example_lwt_command/src/stream/example_stream.ml
@@ -18,12 +18,24 @@ let restart_and_check () =
   Lwt_main.run (restart () >>= (verbose ~f:check))
 
 let () =
+  let r = Lwt_main.run (Process.exec_buffer (Lwt_process.shell "hh_client restart 2>&1")) in
+  match r with
+    | Ok v ->
+      let (s, v) = v in
+      print_endline (string_of_int s)
+    | Error e ->
+      let (s, v) = e in
+      print_endline (string_of_int s)
+
+  (* print_endline v *)
+
+  (*
   let open Unix in
-  (* match Lwt_main.run (Process.wait (Lwt_process.shell "hh_client restart 2>&1") ~f:(fun l -> print_endline l)) with *)
-  match Lwt_main.run (Process.wait (Lwt_process.shell "hhvm --version") ~f:(fun l -> print_endline l)) with
+  match Lwt_main.run (Process.wait (Lwt_process.shell "hh_client restart 2>&1") ~f:(fun l -> print_endline l)) with
     | WEXITED v -> print_endline (string_of_int v); ()
     | WSIGNALED v -> print_endline (string_of_int v); ()
     | WSTOPPED v -> print_endline (string_of_int v); ()
+    *)
 
 (*
   restart_and_check ()
--- example_lwt_command/src/stream/process.ml
+++ example_lwt_command/src/stream/process.ml
@@ -1,13 +1,17 @@
+type process_result = int * string
+
 module type S = sig
   type t
   val exec : Lwt_process.command -> t Lwt.t
   val wait : Lwt_process.command -> f:(string -> unit) -> Unix.process_status Lwt.t
+  val exec_buffer : Lwt_process.command -> (process_result, process_result) result Lwt.t
 end
 
 module Make(S: S) = struct
   type t = S.t
   let wait cmd ~f = S.wait cmd ~f
   let exec cmd = S.exec cmd
+  let exec_buffer = S.exec_buffer
 end
 
 module Process_stream = struct
@@ -21,14 +25,6 @@ module Process_stream = struct
     | EndOfStream of string
 
   let rec read_line cp =
-    (* let read_stdout () = Lwt_io.read_line_opt cp#stdout in *)
-(*
-    let on_raise_error exn =
-      match exn with
-        | Lwt_io.Channel_closed _ -> Lwt.return_none
-        | End_of_file -> Lwt.return_none
-        | e -> raise e
-      in*)
     if Lwt_io.is_busy cp#stdout then
       read_line cp
     else
@@ -36,12 +32,14 @@ module Process_stream = struct
 
   let wait cmd ~f =
     let cp = Lwt_process.open_process_in cmd ~stdin:`Close in
+
     let try_read cp ~f =
       let rec try_read2 cp ~f =
-        let a v = match v with
-          | Some v -> f v; try_read2 cp ~f
-          | None -> Lwt.return_none in
-        read_line cp >>= a in
+        let on_consumed v =
+          match v with
+            | Some v -> f v; try_read2 cp ~f
+            | None -> Lwt.return_none in
+        read_line cp >>= on_consumed in
       try_read2 cp ~f in
 
     let rec wait2 cp ~f =
@@ -54,6 +52,52 @@ module Process_stream = struct
           )
         | Exited status -> Lwt.return status in
     wait2 cp ~f
+
+
+
+
+
+  let read_stdout cp ~f =
+    let rec try_read cp =
+      let on_consumed = function
+        | Some v -> f v; try_read cp
+        | None -> Lwt.return_none in
+
+      read_line cp >>= on_consumed in
+    try_read cp
+
+  let exec_buffer cmd =
+    let buf = Buffer.create 1024 in
+    let writeln s = Buffer.add_string buf s; Buffer.add_char buf '\n' in
+    let cp = Lwt_process.open_process_in cmd ~stdin:`Close in
+
+    let return exit_code =
+      if exit_code = 0 then
+        Lwt.return_ok (exit_code, Buffer.contents buf)
+      else
+        Lwt.return_error (exit_code, Buffer.contents buf) in
+
+    let return_result status =
+      let open Unix in
+      match status with
+        | WEXITED v -> return v
+        | WSIGNALED v -> return v
+        | WSTOPPED v -> return v in
+
+    let rec wait_until_consumed cp =
+      let on_consumed = function
+        | None ->
+          Lwt_io.close cp#stdout
+            >>= (fun _ -> cp#status)
+            >>= return_result
+        | Some _ -> wait_until_consumed cp in
+      match cp#state with
+        | Running -> read_stdout cp ~f:writeln >>= on_consumed
+        | Exited status -> return_result status in
+
+    wait_until_consumed cp
+
+
 
   let exec cmd =
     let buf = Buffer.create 1024 in
