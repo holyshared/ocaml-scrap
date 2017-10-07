ef057d53beb0d4dc4154242a64205588eb6e726d
holyshared <holy.shared.design@gmail.com>
2017-10-07 15:34:25 +0900

    add puts

--- example_jbuilder_with_atdgen/bin/main.ml
+++ example_jbuilder_with_atdgen/bin/main.ml
@@ -1,6 +1,5 @@
 let create_user = User.make
 
 let () =
-  let user = create_user ~name:"foo" ~password:"okgoogle" in
-  print_endline (User.name_of user);
-  print_endline (User.password_of user)
+  let user = User.make ~name:"foo" ~password:"okgoogle" in
+  User.display user
--- example_jbuilder_with_atdgen/src/user.ml
+++ example_jbuilder_with_atdgen/src/user.ml
@@ -6,3 +6,16 @@ let name_of t =
 
 let password_of t =
   User_t.(t.password)
+
+let string_of_user t = Buffer.(
+  let buf = create 1024 in
+    add_string buf "User:\n";
+    add_string buf (Printf.sprintf "  name: %s\n" (name_of t));
+    add_string buf (Printf.sprintf "  password: %s\n" (password_of t));
+    contents buf
+  )
+
+let puts ?(out=stdout) t =
+  output_string out (string_of_user t)
+
+let display t = puts t
