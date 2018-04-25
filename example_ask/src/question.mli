43886f222a8c67eee4a7f0f35f949c814ef1b2bd
holyshared <holy.shared.design@gmail.com>
2018-04-25 13:24:55 +0900

    add option func

--- example_ask/src/question.mli
+++ example_ask/src/question.mli
@@ -3,4 +3,9 @@
 val ask: string -> string
 val ask_map: string -> f:(string -> 'a) -> 'a
 val ask_int: string -> int
+
+val ask_opt: string -> string option
+val ask_require: string -> string
+val ask_opt_int: string -> int option
+
 val next: 'a -> ('a -> 'b) -> 'b
